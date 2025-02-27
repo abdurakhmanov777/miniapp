444881fb2b71d43b440a90d2cca2ff4b4636c50e
abdurakhmanov777 <dalgat03@yandex.ru>
2025-02-27 11:24:33 +0300

    edit

--- js/script.js
+++ js/script.js
@@ -4,14 +4,14 @@ import { handlers } from "./modules/handlers.js";
 const tg = window.Telegram?.WebApp;
 
 // Проверка запуска в Telegram
-if (!tg.initDataUnsafe?.user?.id) {
-    document.body.innerHTML = '<div class="center-message">The site is unavailable outside of Telegram</div>';
-} else {
-    tg.expand();
-    tg.disableVerticalSwipes();
-    // tg.lockOrientation();
+// if (!tg.initDataUnsafe?.user?.id) {
+    // document.body.innerHTML = '<div class="center-message">The site is unavailable outside of Telegram</div>';
+// } else {
+tg.expand();
+tg.disableVerticalSwipes();
+// tg.lockOrientation();
 
-    // Инициализация модулей
-    initializeMenu();
-    handlers();
-}
+// Инициализация модулей
+initializeMenu();
+handlers();
+// }
